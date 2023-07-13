99a2ac29bf07bacc48adff5c9258c026a08c257a
Stanimir Stamenkov <stanio@yahoo.com>
2023-07-13 13:16:58 +0300

    Initial check-in
    Project skeleton generated using Spring Initializr:
    
    <https://start.spring.io/#!type=gradle-project&language=java
    	&platformVersion=3.1.1&packaging=war&jvmVersion=17
    	&groupId=net.example.spring&artifactId=item-list&name=item-list
    	&description=Demo%20project%20for%20Spring%20Boot%20and%20React.js
    	&packageName=net.example.spring&dependencies=web,data-jpa,h2>
    
    Dependencies:
    
    -   Spring Web
    -   Spring Data JPA
    -   H2 Database
    
    Updated:
    
    -   Gradle Wrapper to 8.2.1
    -   Replaced tabs with spaces

---
+++ src/main/java/net/example/spring/ItemListApplication.java
@@ -0,0 +1,13 @@
+package net.example.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItemListApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ItemListApplication.class, args);
+    }
+
+}
---
+++ src/test/java/net/example/spring/ItemListApplicationTests.java
@@ -0,0 +1,13 @@
+package net.example.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItemListApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
