e43c4cf0dbf3711071762ca1f49779149295fbc2
Stanimir Stamenkov <stanio@yahoo.com>
2023-07-17 15:49:19 +0300

    Set up basic frontend sources
    Roughly following:
    
    Use React and Spring Boot to Build a Simple CRUD App
    <https://developer.okta.com/blog/2022/06/17/simple-crud-react-and-spring-boot>
    
    Settle on default application context path: /item-list.  Adjust Gradle build
    output artifact names:
    
    -    item-list-###.war -> item-list-boot.war (:bootWar)
    -    item-list-###-plain.war -> item-list.war (:war)

---
+++ frontend/src/App.jsx
@@ -0,0 +1,41 @@
+import React, { useEffect, useState } from 'react';
+import './App.css';
+
+export default function App() {
+  const [items, setItems] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+
+    fetch('/item-list/api/items')
+      .then(response => response.json())
+      .then(data => {
+        setItems(data);
+        setLoading(false);
+      })
+  }, []);
+
+  if (loading) {
+    return <p>Loading...</p>;
+  }
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src="/item-list/logo.svg" className="App-logo" alt="logo" />
+        Item List
+      </header>
+      <main>
+        <h1>Item List</h1>
+        {items.length ? items.map(it =>
+          <div key={it.id}>
+            {it.label}
+          </div>
+        ) : (
+          <p>(No items.)</p>
+        )}
+      </main>
+    </div>
+  );
+}
---
+++ frontend/src/AppHeader.jsx
@@ -0,0 +1,30 @@
+import React, { useState } from 'react';
+import { Collapse, Nav, Navbar, NavbarBrand, NavbarToggler, NavItem, NavLink } from 'reactstrap';
+import { Link } from 'react-router-dom';
+
+const AppNavbar = () => {
+
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <Navbar color="dark" dark expand="md">
+      <NavbarBrand tag={Link} to="/">
+        <img src="/item-list/logo.svg" className="App-logo" alt="logo" />
+        Item List
+      </NavbarBrand>
+      <NavbarToggler onClick={() => { setIsOpen(!isOpen) }}/>
+      <Collapse isOpen={isOpen} navbar>
+        <Nav className="justify-content-end" style={{width: "100%"}} navbar>
+          <NavItem>
+            <NavLink href="https://twitter.com/oktadev">@oktadev</NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink href="https://github.com/oktadev/okta-spring-boot-react-crud-example">GitHub</NavLink>
+          </NavItem>
+        </Nav>
+      </Collapse>
+    </Navbar>
+  );
+};
+
+export default AppNavbar;
