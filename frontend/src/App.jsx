0600d799f918a71754a98381effe4dfb8deddb45
Stanimir Stamenkov <stanio@yahoo.com>
2023-07-24 15:58:15 +0300

    Lay out the items as table
    -   Show item text and "Action" column
    -   Show "New" action in the caption row
    
    Make some use of reactstrap:
    
    -   https://reactstrap.github.io/

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import './App.css';
+import { Navbar, NavbarBrand, Table } from 'reactstrap';
 
 export default function App() {
   const [items, setItems] = useState([]);
@@ -27,22 +28,73 @@ export default function App() {
     return <p>Loading...</p>;
   }
 
+  const captionRow = {
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+    margin: '0 7px'
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src="/item-list/logo.svg" className="App-logo" alt="logo" />
-        Item List
-      </header>
+    <>
+      <Navbar tag="header" color="secondary" expand="md">
+        <NavbarBrand href="/item-list/">
+          <img src="/item-list/logo.svg" className="App-logo" alt="logo" />
+          {' '}
+          Item List
+	</NavbarBrand>
+      </Navbar>
       <main>
-        <h1>Item List</h1>
-        {items.length ? items.map(it =>
-          <div key={it.id}>
-            {it.label}
-          </div>
-        ) : (
-          <p>(No items.)</p>
-        )}
+        <Table dark responsive striped className="caption-top">
+          <caption>
+            <div style={captionRow}>
+              <span style={{flex: 1}}>Items</span>
+              {' '}
+              <button className="btn btn-success float-end" title="New">
+                <span aria-hidden="true">&#x2795;&#xFE0E;</span>
+              </button>
+            </div>
+          </caption>
+          {items.length ? <ItemRows items={items} />
+                        : <tbody>
+                            <tr>
+                              <td className="text-center">
+                                (No items)
+                              </td>
+                            </tr>
+                          </tbody>}
+        </Table>
       </main>
-    </div>
+    </>
+  );
+}
+
+function ItemRows({ items }) {
+  return (
+    <>
+      <thead>
+        <tr>
+          <th scope="col" className="w-100">Description</th>
+          <th scope="col">Action</th>
+        </tr>
+      </thead>
+      <tbody>
+        {items.map(it =>
+          <tr key={it.id}>
+            <td className="w-100">{it.label}</td>
+            <td>
+              <div className="btn-group">
+                <button type="button" className="btn btn-primary" title="Edit">
+                  <span aria-hidden="true">&#x1F589;&#xFE0E;</span>
+                </button>
+                <button type="button" className="btn btn-danger" title="Delete">
+                  <span aria-hidden="true">&#x1F5D9;&#xFE0E;</span>
+                </button>
+              </div>
+            </td>
+          </tr>
+        )}
+      </tbody>
+    </>
   );
 }
